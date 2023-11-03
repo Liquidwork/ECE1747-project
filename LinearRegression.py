b8ccc432eb72edc4474d80c652cf513e13cdb838
Winter Wang <52688110+Liquidwork@users.noreply.github.com>
2023-11-03 15:20:33 -0400

    comment. the main file

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,11 +1,15 @@
 import numpy as np
 
 def gradientCalculate(X, y, params):
+    ''' Calculate the gradient for a batch of samples.
+        - params: params for the linear regression. The last term is the constant for the
+          linear regression model.
+    '''
     
-    X = np.concatenate((X, np.ones([X.shape[0], 1])), axis=1)
+    X = np.concatenate((X, np.ones([X.shape[0], 1])), axis=1) # Concat column of 1 to the end of matrix X
     params = np.array(params).reshape(-1, 3)
 
-    predict = (X.dot(params.T))
+    predict = (X.dot(params.T)) # Make prediction using current parameter.
     error = predict - y
 
     gradient = 2 * error.T.dot(X) / X.shape[0]
@@ -13,15 +17,17 @@ def gradientCalculate(X, y, params):
     mse = np.mean(error ** 2)
     return gradient, mse
     
-    
+# Generate samples
 X1 = 2 * np.random.rand(1000, 1)
 X2 = -2 * np.random.rand(1000, 1)
 X = np.concatenate((X1, X2), axis=1)
 y = 4 + 3 * X1 + 2 * X2 + np.random.randn(1000, 1)
 
+# Parameters for the regression
 params = [3, 4, 7]
 learning_rate = 0.05
 
+# Fit the model
 for i in range(1000):
     grad, mse = gradientCalculate(X, y, params)
     print(params, grad, mse)
