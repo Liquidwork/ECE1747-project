c7452490076381a760b3dbad88ed212bed878c19
Winter Wang <52688110+Liquidwork@users.noreply.github.com>
2023-11-05 20:28:06 -0500

    fix. params shape

--- LinearRegression.py
+++ LinearRegression.py
@@ -7,7 +7,7 @@ def gradientCalculate(X, y, params):
     '''
     
     X = np.concatenate((X, np.ones([X.shape[0], 1])), axis=1) # Concat column of 1 to the end of matrix X
-    params = np.array(params).reshape(-1, 3)
+    params = np.array(params).reshape(1, -1)
 
     predict = (X.dot(params.T)) # Make prediction using current parameter.
     error = predict - y
@@ -17,21 +17,21 @@ def gradientCalculate(X, y, params):
     mse = np.mean(error ** 2)
     return gradient, mse
     
-# Generate samples
-X1 = 2 * np.random.rand(1000, 1)
-X2 = -2 * np.random.rand(1000, 1)
-X = np.concatenate((X1, X2), axis=1)
-y = 4 + 3 * X1 + 2 * X2 + np.random.randn(1000, 1)
+# # Generate samples
+# X1 = 2 * np.random.rand(1000000, 1)
+# X2 = -2 * np.random.rand(1000000, 1)
+# X = np.concatenate((X1, X2), axis=1)
+# y = 4 + 3 * X1 + 2 * X2 + np.random.randn(1000000, 1)
 
-# Parameters for the regression
-params = [3, 4, 7]
-learning_rate = 0.05
+# # Parameters for the regression
+# params = [3, 4, 7]
+# learning_rate = 0.05
 
-# Fit the model
-for i in range(1000):
-    grad, mse = gradientCalculate(X, y, params)
-    print(params, grad, mse)
-    # Change gradient according to learning rate
-    params -= learning_rate * grad
+# # Fit the model
+# for i in range(1000):
+#     grad, mse = gradientCalculate(X, y, params)
+#     print(params, grad, mse)
+#     # Change gradient according to learning rate
+#     params -= learning_rate * grad
     
     
