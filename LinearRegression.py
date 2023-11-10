0836a830b653b6b275eea01d11a98a298d2dc7a5
Winter Wang <52688110+Liquidwork@users.noreply.github.com>
2023-11-09 23:51:41 -0500

    fix. flatten output and standard y

--- LinearRegression.py
+++ LinearRegression.py
@@ -7,6 +7,7 @@ def gradientCalculate(X, y, params):
     '''
     
     X = np.concatenate((X, np.ones([X.shape[0], 1])), axis=1) # Concat column of 1 to the end of matrix X
+    y = np.reshape(1, -1)
     params = np.array(params).reshape(1, -1)
 
     predict = (X.dot(params.T)) # Make prediction using current parameter.
@@ -15,7 +16,7 @@ def gradientCalculate(X, y, params):
     gradient = 2 * error.T.dot(X) / X.shape[0]
     
     mse = np.mean(error ** 2)
-    return gradient, mse
+    return gradient.flatten(), mse
     
 # # Generate samples
 # X1 = 2 * np.random.rand(1000000, 1)
